81d8c4300bc53b9dd385cb58c695cc6ce36de2d8
中丸隆寛 <nakamaruomaru0101@gmail.com>
2016-02-05 17:01:55 +0900

    moromoro

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'tweets#index' #ツイート一覧画面
   get 'tweets' =>'tweets#index'
   get 'tweets/new' =>'tweets#new' #ツイート新規作成画面
---
+++ db/migrate/20160205073920_addnickname_to_users.rb
@@ -0,0 +1,4 @@
+class AddnicknameToUsers < ActiveRecord::Migration
+  def change
+  end
+end
