5c7dca5142ede27d1692ef1b4c510ed99f04088b
中丸隆寛 <nakamaruomaru0101@gmail.com>
2016-02-05 15:39:27 +0900

    pagination追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
+  root 'tweets#index' #ツイート一覧画面
+  get 'tweets' =>'tweets#index'
+  get 'tweets/new' =>'tweets#new' #ツイート新規作成画面
+  post 'tweets'=> 'tweets#create' #ツイート投稿画面
 end
